44adcd29c1198e50eae064528963b484bcf63cc6
Júlia Moreira Gregis dos Reis <85226203004@senacrs.edu.br>
2024-08-16 11:10:01 -0300

    oii
    oii

---
+++ script.js
@@ -0,0 +1,12 @@
+// perguntado ao usuário
+let nome = prompt("Digite seu nome:");
+alert("Olá " +nome+ ", tudo bem?");
+
+// alerta na tela
+alert("Olá Mundo!");
+
+// console do navegador
+console.log("Teste de console");
+
+// inclui um html em um elemento identificado pelo ID
+document.getElementById("demo").innerHTML = "Júlia Moreira Gregis dos Reis";
